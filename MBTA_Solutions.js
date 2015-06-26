dbb082ec03c5a72352cfc817538dfd1b727a46db
tiffboors29 <tiffanyboorsma@gmail.com>
2015-06-26 09:43:14 -0400

    add tester function with tester cases
    same line function works with tests

--- MBTA_Solutions.js
+++ MBTA_Solutions.js
@@ -38,14 +38,22 @@ green = [
 
 
 
-var sameLine = function( start, stop, line) {
-
+var sameLine = function(line, firstStop, secondStop) {
+  //finde indices of start and stop and subtract them
+  //only care about magnitude, not direction, so take absolute value
+  var diff = find(line, firstStop) - find(line, secondStop);
+  if (diff < 0) {
+    diff *= -1;
+  }
+  return diff;
+  //using a ternary operator instead of if would be
+  // return (diff > 0)? diff : -diff;
 };
 
 
 
 var find = function(array, value) { //find value
-  for (i = 0; i < array.length; i++) {
+  for (var i = 0; i < array.length; i++) {
     if (array[i] === value) {
       return [i];
       }
@@ -56,6 +64,16 @@ var find = function(array, value) { //find value
 
 
 var tester = function() {
+  console.log('find');
   console.log('Find should return 2, actually returns ' + find(['a', 'b', 'c'], 'c'));
-console.log('Find should return -1, actually returns ' + find(['a', 'b', 'c'], 'z'));
+  console.log('Find should return -1, actually returns ' + find(['a', 'b', 'c'], 'z'));
+  console.log('sameLine');
+  console.log('* should return 1, actually returns ' + sameLine(red, 'south station', 'park st'));
+  console.log('* should return 4, actually returns ' + sameLine(red, 'kendall', 'davis'));
+  console.log('* should return 1, actually returns ' + sameLine(orange, 'back bay', 'forest hills'));
+  console.log('* should return 3, actually returns ' + sameLine(orange, 'north station', 'state'));
+  console.log('* should return 1, actually returns ' + sameLine(green, 'arlington', 'copley'));
+  console.log('* should return 5, actually returns ' + sameLine(green, 'haymarket', 'copley'));
 };
+
+tester();
