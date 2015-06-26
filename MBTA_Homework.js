01fb2d6c6eaa53f84f76d25eaa9612c87f816a1a
tiffboors29 <tiffanyboorsma@gmail.com>
2015-06-26 09:52:45 -0400

    Added tester function to homework file for my own solution
    tests created for same line routes and changing lines

--- MBTA_Homework.js
+++ MBTA_Homework.js
@@ -1,8 +1,35 @@
 'use strict;'
 
-var red = ['south station', 'park st', 'kendall', 'central', 'harvard', 'porter', 'davis', 'alewife'];
-var green = ['haymarket', 'government center', 'park st', 'boylston', 'arlington', 'copley'];
-var orange = ['north station', 'haymarket', 'park st', 'state', 'downtown crossing', 'chinatown', 'back bay', 'forest hills'];
+var red = [
+  'south station',
+  'park st',
+  'kendall',
+  'central',
+  'harvard',
+  'porter',
+  'davis',
+  'alewife'
+  ];
+
+var green = [
+  'haymarket',
+  'government center',
+  'park st',
+  'boylston',
+  'arlington',
+  'copley'
+  ];
+
+var orange = [
+  'north station',
+  'haymarket',
+  'park st',
+  'state',
+  'downtown crossing',
+  'chinatown',
+  'back bay',
+  'forest hills'
+  ];
 
 var stopsBetween = function(startLine, startStation, stopLine, stopStation) {
   var totalStops;
@@ -20,3 +47,21 @@ var stopsBetween = function(startLine, startStation, stopLine, stopStation) {
     return totalStops;
   }
 };
+
+
+var tester = function() {
+  console.log('stops between on same line');
+  console.log('* should return 1, actually returns ' + stopsBetween(red, 'south station', red, 'park st'));
+  console.log('* should return 4, actually returns ' + stopsBetween(red, 'kendall', red, 'davis'));
+  console.log('* should return 1, actually returns ' + stopsBetween(orange, 'back bay', orange, 'forest hills'));
+  console.log('* should return 3, actually returns ' + stopsBetween(orange, 'north station', orange, 'state'));
+  console.log('* should return 1, actually returns ' + stopsBetween(green, 'arlington', green, 'copley'));
+
+  console.log('stops between different lines');
+  console.log('* should return 5, actually returns ' + stopsBetween(green, 'arlington', red, 'harvard'));
+  console.log('* should return 1, actually returns ' + stopsBetween(red, 'south station', orange, 'park st'));
+  console.log('* should return 3, actually returns ' + stopsBetween(orange, 'north station', green, 'boylston'));
+  console.log('* should return 6, actually returns ' + stopsBetween(green, 'arlington', red, 'porter'));
+};
+
+tester();
